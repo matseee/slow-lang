9801f1d59fd54a986b5f203cf0d685c8e4b9b538
Matthias Fischer <m@tthias.space>
2023-08-16 18:07:53 +0200

    add basic abstract-syntax-tree elements

---
+++ src/ast/ast.go
@@ -0,0 +1,51 @@
+package ast
+
+// Abstract Syntax Tree
+
+import "slow-lang/token"
+
+// let <identifier> = <expression>
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program implementation - root node
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// Let statement implementation
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
--- src/repl/repl.go
+++ src/repl/repl.go
@@ -13,7 +13,7 @@ const PROMPT = ">> "
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	for {
-		fmt.Fprintf(out, PROMPT)
+		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
 
 		if !scanned {
