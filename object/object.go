034eb60483e65a38c3774edd6f9060b0eab1fff2
Matthias Fischer <m@tthias.space>
2025-09-14 13:45:06 +0200

    add bultin len function

--- object/object.go
+++ object/object.go
@@ -24,6 +24,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Boolean struct {
@@ -90,3 +91,12 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin_function" }
